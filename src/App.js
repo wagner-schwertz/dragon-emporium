5dfc0023e348412e2e557c4e5bdd87a2310d44f6
wagnerschwertz <wagner.santos@ntconsult.com.br>
2021-02-02 17:56:12 -0300

    chore: CRA housekeeping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Routes from "routes";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "styles/GlobalStyles";
+import Toast from "components/Toast";
+import { useStore } from "store/store";
+import themes from "styles/themes";
 
 function App() {
+  const theme = useStore((store) => store.theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <Routes />
+      <GlobalStyles />
+      <Toast />
+    </ThemeProvider>
   );
 }
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,24 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+        language: "en",
+      },
+    };
+  },
+}));
