1f428847079753a857040b2fc10877197665cbe0
wagnerschwertz <wagner.santos@ntconsult.com.br>
2021-02-02 17:54:38 -0300

    added: theme

---
+++ src/styles/themes.js
@@ -0,0 +1,20 @@
+const themes = {
+  violet: {
+    colors: {
+      primary: "#7900b5",
+      success: "#11791f",
+      error: "#861d1d",
+      background: "#201924",
+    },
+  },
+  navy: {
+    colors: {
+      primary: "#0039b5",
+      success: "#11791f",
+      error: "#861d1d",
+      background: "#191a24",
+    },
+  },
+};
+
+export default themes;
